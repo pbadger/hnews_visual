3459c6964721804e22b579a5ed3914a786eca4cc
Peregrine Badger <pbadger@college.harvard.edu>
2013-04-28 21:31:18 -0400

    setup_themes

---
+++ app/assets/javascripts/setup_themes.js
@@ -0,0 +1,14 @@
+themes = {};
+themes.bmbombs = {};
+themes.cispa = {};
+themes.google_glass = {};
+themes.startups = {};
+
+themes.bmbombs.keywords = ['boston','bombing','bomb','bombs','marathon','dead',
+'terrorism','tsarnaev','reddit','police','mit'];
+
+themes.cispa.keywords = ['cispa','privacy'];
+
+themes.google_glass.keywords = ['google','glass','glasses'];
+
+themes.startups.keywords = ['startups','startup','founder','founders','funding','venture','capital'];
