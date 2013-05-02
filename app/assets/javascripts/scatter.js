02fe21cda71961a35173bc8a154d4ac385a47d24
Brian Connolly <brianc217@gmail.com>
2013-05-02 17:43:31 -0400

    rearranged

--- app/assets/javascripts/scatter.js
+++ app/assets/javascripts/scatter.js
@@ -161,10 +161,9 @@ function draw_scatter(theme){
     $('.scatter_plot .tick')[0].remove()
 
     $('.scatter_plot a').mouseover(function(e){
-      var mouse_top = e.clientY;
-      var mouse_left = e.clientX;
+      var mouse_top = e.pageY;
+      var mouse_left = e.pageX;
       $('#scatter_tt').offset({top: mouse_top-50, left: mouse_left+5})
     });
-
 
 };
